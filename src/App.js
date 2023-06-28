7e13237c0d6704c6dce72e3a94680c1e943d4514
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-06-28 23:54:07 +0530

    usecontext

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ComA from "./ComA";
+import { createContext } from "react";
+
+
+const FirstName = createContext();
+  const LastName = createContext();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FirstName.Provider value={'manisha'}>
+        <LastName.Provider value={'nayak'}>
+          <ComA />
+        </LastName.Provider>
+      </FirstName.Provider>
     </div>
   );
 }
 
 export default App;
+export { FirstName, LastName };
---
+++ src/ComB.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import ComC from './ComC'
+import { FirstName, LastName } from './App'
+
+const ComB = () => {
+    const fname = useContext(FirstName);
+    const lname = useContext(LastName);
+  return (
+    <div>
+         <h1>My name is {fname} {lname}</h1>
+    </div>
+  )
+}
+
+export default ComB
